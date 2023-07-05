5e3416e05f1c42518778e403467ea01bd86af19b
Rajat Gupta <dguptarajat@gmail.com>
2023-07-05 18:10:12 +0530

    changed-base-server-url

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://hopegivers-server.vercel.app";
 
 export default axios.create({
   baseURL: BASE_URL,
